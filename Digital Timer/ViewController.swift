9c598a2f4395ef6e7fb22dd0459f0ff832068afc
D7703_28 <d7703_28@d7703-28ui-imac.local>
2018-04-09 14:46:24 +0900

    timersimple

--- Digital Timer/ViewController.swift
+++ Digital Timer/ViewController.swift
@@ -11,20 +11,32 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timerSwitch: UISwitch!
     var myTimer = Timer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        
+        timerSwitch.setOn(false, animated: true)
+    //    myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
     }
     
     @objc func updateTime() {
         let date = Date()
         let formatter = DateFormatter()
-        formatter.dateFormat = "HH:mm:ss EEE"
+        formatter.dateFormat = "HH:mm:ss a"
         timeLabel.text = formatter.string(from: date)
+        // timeLabel.text = String(describing: date)
     }
 
+    @IBAction func timerStart(_ sender: Any) {
+        if timerSwitch.isOn == true {
+            myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+            
+        }else {
+            myTimer.invalidate()
+        }
+    }
 }
 
